c84c83523c657a3a887efd7b4eb5db58786f61bc
Kevin Hu <hellokevinhu@icloud.com>
2024-06-13 00:03:06 +0800

    Complete CRUD functionality for post

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -17,7 +17,7 @@ export const createPost: RequestHandler = async (req, res) => {
     },
   });
 
-  res.status(201).json({ message: 'Created a new post.', postId: post.id });
+  res.status(201).json({ message: 'Post create successful.', postId: post.id });
 };
 
 export const getPostById: RequestHandler = async (req, res) => {
@@ -28,5 +28,104 @@ export const getPostById: RequestHandler = async (req, res) => {
     return sendErrorRes(res, 'Post not found.', 404);
   }
 
-  res.json({ post: post });
+  res.json(post);
+};
+
+export const getPosts: RequestHandler = async (req, res) => {
+  const {
+    userId,
+    category,
+    postId,
+    searchTerm,
+    sortDirection = 'desc',
+    startIndex = 0,
+    limit = 9,
+  } = req.query;
+
+  const whereClause: any = {};
+
+  if (userId) {
+    whereClause.userId = Number(userId);
+  }
+
+  if (category) {
+    whereClause.category = category;
+  }
+
+  if (postId) {
+    whereClause.id = Number(postId);
+  }
+
+  if (searchTerm) {
+    whereClause.OR = [
+      { title: { contains: searchTerm } },
+      { content: { contains: searchTerm } },
+    ];
+  }
+
+  const validSortDirections: Array<'asc' | 'desc'> = ['asc', 'desc'];
+  const sortDirectionStr = validSortDirections.includes(
+    sortDirection as 'asc' | 'desc'
+  )
+    ? (sortDirection as 'asc' | 'desc')
+    : 'desc';
+
+  const posts = await PostModel.findMany({
+    where: whereClause,
+    orderBy: {
+      updatedAt: sortDirectionStr,
+    },
+    skip: Number(startIndex),
+    take: Number(limit),
+  });
+
+  res.json(posts);
+};
+
+export const deletePost: RequestHandler = async (req, res) => {
+  const postId = req.params.id;
+
+  const post = await PostModel.findUnique({ where: { id: postId } });
+  if (!post) {
+    return sendErrorRes(res, 'Post not found.', 404);
+  }
+
+  if (req.user.role === 'TEACHER' && req.user.id !== post.authorId) {
+    return sendErrorRes(res, 'You are not allowed to delete this post.', 403);
+  }
+
+  await PostModel.delete({
+    where: {
+      id: postId,
+    },
+  });
+
+  res.json({ message: 'Post delete successful.' });
+};
+
+export const updatePost: RequestHandler = async (req, res) => {
+  const postId = req.params.id;
+  const { title, category, content, image } = req.body;
+
+  const post = await PostModel.findUnique({ where: { id: postId } });
+  if (!post) {
+    return sendErrorRes(res, 'Post not found.', 404);
+  }
+
+  if (req.user.role === 'TEACHER' && req.user.id !== post.authorId) {
+    return sendErrorRes(res, 'You are not allowed to update this post.', 403);
+  }
+
+  await PostModel.update({
+    where: { id: postId },
+    data: {
+      title,
+      category,
+      content,
+      image,
+      updatedAt: new Date(),
+    },
+  });
+
+  res.json({ message: 'Post update successful.' });
 };
--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express';
-import { createPost, getPostById } from 'src/controllers/post';
+import {
+  createPost,
+  deletePost,
+  getPostById,
+  getPosts,
+  updatePost,
+} from 'src/controllers/post';
 import { verifyRole, verifyToken } from 'src/middleware/auth';
 import { validate } from 'src/middleware/validator';
 import { postCreateSchema } from 'src/validation/schema/post';
@@ -13,6 +19,19 @@ postRouter.post(
   validate(postCreateSchema),
   createPost
 );
-postRouter.get('/:id', getPostById);
+postRouter.get('/detail/:id', getPostById);
+postRouter.get('/get-posts', getPosts);
+postRouter.delete(
+  '/delete/:id',
+  verifyToken,
+  verifyRole(['ADMIN', 'TEACHER']),
+  deletePost
+);
+postRouter.patch(
+  '/update/:id',
+  verifyToken,
+  verifyRole(['ADMIN', 'TEACHER']),
+  updatePost
+);
 
 export default postRouter;
